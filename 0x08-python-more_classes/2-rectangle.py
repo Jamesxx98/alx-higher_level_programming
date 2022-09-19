cad68e4750b155ac7f76b5b0d035aa66c647c696
Jamesxx98 <Jamesmaina445@gmail.com>
2022-09-19 14:03:38 -0700

    rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,10 +4,9 @@
 
 class Rectangle:
     """Represent a rectangle."""
-
     def __init__(self, width=0, height=0):
     """Initialize a new Rectangle.
-
+    
     Args:
     width (int): The width of the new rectangle.
     height (int): The height of the new rectangle.
@@ -17,7 +16,7 @@ class Rectangle:
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -30,13 +29,23 @@ class Rectangle:
 
         @property
         def height(self):
-            """Get/set the height of the rectangle."""
+            """Get/set the height of the Rectangle."""
             return self.__height
-
+        
         @height.setter
         def height(self, value):
             if not isinstance(value, int):
                 raise TypeError("height must be an integer")
             if value < 0:
                 raise ValueError("height must be >= 0")
-            self.__height = 
+            self.__height = value
+            
+            def area(self):
+                """Return the area of the Rectangle."""
+                return (self.__width * self.__height)
+            
+            def perimeter(self):
+                """Return the perimeter of the Rectangle."""
+                if self.__width == 0 or self.__height == 0:
+                    return (0)
+                return ((self.__width * 2) + (self.__height * 2))
